97880f882b0917e555aa06cc91d21cc05489f5ba
sjaaishah <sitiaishahjupri@gmail.com>
2024-11-13 15:48:29 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -7,6 +7,14 @@ st.header("My first Streamlit App")
 option = st.sidebar.selectbox(
     'Select a mini project',
      ['line chart','map','T n C'])
+if option=='line chart':
+    chart_data = pd.DataFrame(
+      np.random.randn(20, 3),
+      columns=['a', 'b', 'c'])
+    st.line_chart(chart_data)
+
+st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
+show = st.checkbox('I agree the terms and conditions')
 st.write(pd.DataFrame({
     'Students': ['John', 'Lofa', 'Siti', 'Amy'],
     'Attendance Status': ['yes', 'yes', 'yes', 'no']
